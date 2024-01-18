b919488199578e78717b698ec0c4932396796964
Daniella <damendaniella@gmail.com>
2024-01-18 22:04:35 +0100

    loader and results added

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -91,9 +91,9 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
 			<Box sx={{ position: "relative", width: "100%", p: "20px" }}>
 				<HorizontalScrollbar
 					data={bodyParts}
-					bodyParts
-					setBodyPart={setBodyPart}
 					bodyPart={bodyPart}
+					setBodyPart={setBodyPart}
+					isBodyParts
 				/>
 			</Box>
 		</Stack>
